325e412b2c4fffe41c159204ff79c85af5582b0f
Fabian Suter <fabian.suter@protonx.ch>
2025-08-23 14:14:47 +0200

    Adjust Program.cs

--- DesignPatternApp/Program.cs
+++ DesignPatternApp/Program.cs
@@ -1,165 +1,170 @@
-﻿namespace ch.px.designpattern
+﻿namespace ch.px.designpattern;
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            // ==================================================
-            // Behavioural Patterns
-            // ==================================================
+        // ==================================================
+        // Behavioural Patterns
+        // ==================================================
 
-            // --------------------------------------------------
-            // Memento Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Memento Pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.Memento.GoodSolution.Start.Run();
+        // Behavioral.Memento.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // State Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // State Pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.StatePattern.BadSolution.Start.Run();
-            // Behavioral.StatePattern.GoodSolution.Start.Run();
+        // Behavioral.StatePattern.BadSolution.Start.Run();
+        // Behavioral.StatePattern.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Observer Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Observer Pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.Observer.BadSolution.Start.Run();
-            // Behavioral.Observer.GoodSolution.Start.Run();
-            // Behavioral.Observer.GoodSolutionPull.Start.Run();
-            // Behavioral.Observer.GoodSolutionPush.Start.Run();
+        // Behavioral.Observer.BadSolution.Start.Run();
+        // Behavioral.Observer.GoodSolution.Start.Run();
+        // Behavioral.Observer.GoodSolutionPull.Start.Run();
+        // Behavioral.Observer.GoodSolutionPush.Start.Run();
 
-            // --------------------------------------------------
-            // Template method Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Template method Pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.TemplateMethod.BadSolution.Start.Run();
-            // Behavioral.TemplateMethod.GoodSolution.Start.Run();
-            // Behavioral.TemplateMethod.StrategyPattern.Start.Run();
+        // Behavioral.TemplateMethod.BadSolution.Start.Run();
+        // Behavioral.TemplateMethod.GoodSolution.Start.Run();
+        // Behavioral.TemplateMethod.StrategyPattern.Start.Run();
 
-            // --------------------------------------------------
-            // Mediator Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Mediator Pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.Mediator.GoodSolution.Start.Run();
+        // Behavioral.Mediator.GoodSolution.Start.Run();
 
-            // Strategy Pattern - behavioural - Module04
-            // 03:00:30
+        // Strategy Pattern - behavioural - Module04
+        // 03:00:30
 
-            // Iterator pattern - behavioural - Module05
+        // Iterator pattern - behavioural - Module05
 
-            // Command pattern - behavioural - Module06
+        // Command pattern - behavioural - Module06
 
-            // --------------------------------------------------
-            // Template method Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Template method Pattern - behavioural
+        // --------------------------------------------------
 
-            // TemplateMethod.BadSolution.Start.Run();
-            // TemplateMethod.StrategyPattern.Start.Run();
-            // TemplateMethod.GoodSolution.Start.Run();
+        // TemplateMethod.BadSolution.Start.Run();
+        // TemplateMethod.StrategyPattern.Start.Run();
+        // TemplateMethod.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Chain Of Responsibilty pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Chain Of Responsibilty pattern - behavioural
+        // --------------------------------------------------
 
-            // Behavioral.ChainOfResponsibilty.BadSolution.Start.Run();
-            // Behavioral.ChainOfResponsibilty.GoodSolution.Start.Run();
+        // Behavioral.ChainOfResponsibilty.BadSolution.Start.Run();
+        // Behavioral.ChainOfResponsibilty.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Interpreter Pattern - behavioural
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Visitor Pattern - behavioural
+        // --------------------------------------------------
 
-            Behavioral.Interpreter.BadSolution.Start.Run();
-            Behavioral.Interpreter.GoodSolution.Start.Run();
+        // Behavioral.Visitor.BadSolution.Start.Run();
+        Behavioral.Visitor.GoodSolution.Start.Run();
 
-            // ==================================================
-            // Structural Patterns
-            // ==================================================
+        // --------------------------------------------------
+        // Interpreter Pattern - behavioural
+        // --------------------------------------------------
 
-            // --------------------------------------------------
-            // Composit pattern - structural pattern
-            // --------------------------------------------------
+        // Behavioral.Interpreter.BadSolution.Start.Run();
+        // Behavioral.Interpreter.GoodSolution.Start.Run();
 
-            // Structural.Composite.BadSolution.Start.Run();
-            // Structural.Composite.GoodSolution.Start.Run();
+        // ==================================================
+        // Structural Patterns
+        // ==================================================
 
-            // --------------------------------------------------
-            // Adapter pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Composit pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Adapter.GoodSolution.Start.Run();
+        // Structural.Composite.BadSolution.Start.Run();
+        // Structural.Composite.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Bridge pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Adapter pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Bridge.BadSolution.Start.Run();
-            // Structural.Bridge.GoodSolution.Start.Run();
+        // Structural.Adapter.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Proxy pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Bridge pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Proxy.BadSolution.Start.Run();
-            // Structural.Proxy.GoodSolution.Start.Run();
+        // Structural.Bridge.BadSolution.Start.Run();
+        // Structural.Bridge.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Flyweight pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Proxy pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Flyweight.BadSolution.Start.Run();
-            // Structural.Flyweight.GoodSolution.Start.Run();
+        // Structural.Proxy.BadSolution.Start.Run();
+        // Structural.Proxy.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Facade pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Flyweight pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Facade.BadSolution.Start.Run();
-            // Structural.Facade.GoodSolution.Start.Run();
+        // Structural.Flyweight.BadSolution.Start.Run();
+        // Structural.Flyweight.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Decorator pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Facade pattern - structural pattern
+        // --------------------------------------------------
 
-            // Structural.Decorator.BadSolution.Start.Run();
-            // Structural.Decorator.GoodSolution.Start.Run();
+        // Structural.Facade.BadSolution.Start.Run();
+        // Structural.Facade.GoodSolution.Start.Run();
 
-            // ==================================================
-            // Creational Patterns
-            // ==================================================
+        // --------------------------------------------------
+        // Decorator pattern - structural pattern
+        // --------------------------------------------------
 
-            // --------------------------------------------------
-            // Prototype pattern - structural pattern
-            // --------------------------------------------------
+        // Structural.Decorator.BadSolution.Start.Run();
+        // Structural.Decorator.GoodSolution.Start.Run();
 
-            // Creational.Prototype.BadSolution.Start.Run();
-            // Creational.Prototype.GoodSolution.Start.Run();
+        // ==================================================
+        // Creational Patterns
+        // ==================================================
 
-            // --------------------------------------------------
-            // Singleton pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Prototype pattern - structural pattern
+        // --------------------------------------------------
 
-            // Creational.Singleton.BadSolution.Start.Run();
-            // Creational.Singleton.GoodSolution.Start.Run();
+        // Creational.Prototype.BadSolution.Start.Run();
+        // Creational.Prototype.GoodSolution.Start.Run();
 
-            // --------------------------------------------------
-            // Factory method pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Singleton pattern - structural pattern
+        // --------------------------------------------------
 
-            // Creational.FactoryMethod.BadSolution.Start.Run();
-            // Creational.FactoryMethod.GoodSolution.Start.Run();
+        // Creational.Singleton.BadSolution.Start.Run();
+        // Creational.Singleton.GoodSolution.Start.Run();
+
+        // --------------------------------------------------
+        // Factory method pattern - structural pattern
+        // --------------------------------------------------
+
+        // Creational.FactoryMethod.BadSolution.Start.Run();
+        // Creational.FactoryMethod.GoodSolution.Start.Run();
 
 
 
 
-            // --------------------------------------------------
-            // Builder pattern - structural pattern
-            // --------------------------------------------------
+        // --------------------------------------------------
+        // Builder pattern - structural pattern
+        // --------------------------------------------------
 
-            // Creational.Builder.BadSolution.Start.Run();
-            // Creational.Builder.GoodSolution.Start.Run();
-            // Creational.Builder.GoodSolutionDirector.Start.Run();
-        }
+        // Creational.Builder.BadSolution.Start.Run();
+        // Creational.Builder.GoodSolution.Start.Run();
+        // Creational.Builder.GoodSolutionDirector.Start.Run();
     }
 }
